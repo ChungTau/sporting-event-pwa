621f6c82867acda86c068cc1ca6cac66f9fc1567
ChungTau <edwardonionc020903@gmail.com>
2024-01-12 04:20:31 +0800

    Add backend

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import sequelize from './config/dbConfig';
+import userRoutes from './routes/userRoutes';
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+sequelize.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.error('Unable to connect to the database:', err));
+
+sequelize.sync();
+
+app.use(express.json());
+app.use('/api', userRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});

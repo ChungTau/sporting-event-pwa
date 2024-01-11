d0c7c5e2853acafe5747f9ed6d5ccbd6482212ff
ChungTau <edwardonionc020903@gmail.com>
2024-01-12 06:15:39 +0800

    Apply log for index

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,34 +1,39 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const http = __importStar(require("http"));
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+const express_1 = __importDefault(require("express"));
+//import sequelize from './config/dbConfig';
+//import userRoutes from './routes/userRoutes';
+const dotenv_1 = __importDefault(require("dotenv"));
+//For env File 
+dotenv_1.default.config();
+// Catch unhandled promise rejections
+process.on('unhandledRejection', error => {
+    console.error('Unhandled Promise Rejection:', error);
 });
-const PORT = 5001;
-server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
+// Catch uncaught exceptions
+process.on('uncaughtException', error => {
+    console.error('Uncaught Exception:', error);
 });
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+/*sequelize.authenticate()
+  .then(() => {
+    console.log('Database connected...');
+
+    sequelize.sync();
+
+    app.use(express.json());
+    app.use('/api', userRoutes);
+
+    
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+*/ 

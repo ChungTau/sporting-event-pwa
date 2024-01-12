24a35daf19e178637dd173d77cae39baff75f190
ChungTau <edwardonionc020903@gmail.com>
2024-01-12 08:50:33 +0800

    Rebase backend

--- backend/src/routes/userRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import * as userController from '../controllers/userController';
-
-const router = Router();
-
-router.post('/users', userController.createUser);
-router.get('/users/:id', userController.getUserById);
-export default router;

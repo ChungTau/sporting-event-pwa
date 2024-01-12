e9104a70f6eb0b695ba34f5bc57b5b9a52a75ebd
ChungTau <edwardonionc020903@gmail.com>
2024-01-12 10:11:00 +0800

    Rebase backend

--- backend/src/config/dbConfig.ts
+++ backend/src/config/dbConfig.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize-typescript';
 
-const sequelize = new Sequelize(process.env.DB_NAME!, process.env.DB_USER!, process.env.DB_PASS!, {
-  host: process.env.DB_HOST,
+const sequelize = new Sequelize('FYP', 'root', '1234', {
+  host: 'localhost',
   dialect: 'mysql',
   models: [__dirname + '/../models'],
   logging: console.log,

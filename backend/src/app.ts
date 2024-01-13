5ed6b76ecb8b6b928a392de771d6c534fcdb5591
ChungTau <edwardonionc020903@gmail.com>
2024-01-14 01:23:52 +0800

    Add swagger

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -26,9 +26,10 @@ sequelize.authenticate()
     console.log('Database connected...');
 
     sequelize.sync();
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+    
     app.use(express.json());
     app.use(cors());
+    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
     app.use('/api', userRoutes);
 
     app.listen(PORT, () => {

0b92eb3ef88335117857d5a49b164a7d40478bab
ChungTau <edwardonionc020903@gmail.com>
2024-01-15 21:12:46 +0800

    Migration to Raspberry

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -19,7 +19,7 @@ process.on('uncaughtException', error => {
 });
 
 const app: Application = express();
-const PORT = process.env.PORT || 3389;
+const PORT = process.env.PORT || 8080;
 
 sequelize.authenticate()
   .then(() => {

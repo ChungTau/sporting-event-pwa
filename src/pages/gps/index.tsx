12ce800be8f9f17596378841a4df6fd48daf3a48
Your Name <you@example.com>
2024-01-19 01:12:06 +0800

    x

--- src/pages/gps/index.tsx
+++ src/pages/gps/index.tsx
@@ -5,12 +5,13 @@ import {fadeVariants} from "../../constants/animateVariant";
 // Code9
 import { useState } from "react";
 import ReactDOM from 'react-dom/client';
+import 'mapbox-gl/dist/mapbox-gl.css';
+import mapboxgl from 'mapbox-gl';
+
+
 
 
 // Code9 Temporary
-
-
-
 function GPSPage() {
 
 	function getLocation(){
@@ -45,6 +46,34 @@ function GPSPage() {
 		const [lat, setLat] = useState(0);
 		const [long, setLong] = useState(0);
 		// const long = 0;
+		
+   mapboxgl.accessToken = 'pk.eyJ1IjoiaGt1c3RnaWI1IiwiYSI6ImNscWllMDN2ZzFzOG0ya21kbThtc2p5aDYifQ.VqYPzJ_2tRigF5thlG8A-w'; 
+    const map = new mapboxgl.Map({
+      container: 'map',
+      // Replace YOUR_STYLE_URL with your style URL.
+      style: 'mapbox://styles/hkustgib5/clqv45j5n00y301qu4wwp9poe', 
+      center: [114.2, 22.4],
+      zoom: 18
+    });
+		
+		var geolocate = new mapboxgl.GeolocateControl();
+		var isLogging = false;
+
+		map.addControl(geolocate);
+
+		geolocate.on('geolocate', function(e) {
+					var lon = e.coords.longitude;
+					var lat = e.coords.latitude
+					var position = [lon, lat];
+					// console.log(position);
+					document.getElementById("coordinates").innerHTML = `${lon}, ${lat}`;
+					map.setZoom(14);
+		});
+		
+		setInterval(function() {
+    $(".mapboxgl-ctrl-geolocate").click();
+		console.log("Clicked");
+		},5000);
 
     return (
         <motion.div
@@ -61,6 +90,7 @@ function GPSPage() {
             <Center>Get your GPS Coordinates:</Center>
             <Center>Coordinates: {lat} {long} {color}</Center>
 						<Center><button onClick={getLocation}>Click</button></Center>
+						<div id="map">test</div>
         </motion.div>
     );
 };

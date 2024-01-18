d1b5048fe40e19b1841202b726eb34a9553cdc62
Edward Chan <edward.chan@wisespotgroup.com>
2024-01-18 16:13:52 +0800

    Add missing package

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,10 +1,10 @@
-interface User {
+import { BaseModel } from "./BaseModel";
+
+interface User extends BaseModel{
     id: number;
     username: string;
     email: string;
     password: string;
-    createdAt?: Date;
-    updatedAt?: Date;
   }
   
   export default User;

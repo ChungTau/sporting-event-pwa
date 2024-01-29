1a2bfa6e72aa2ef852b6b44869fe7e6ae226ac3f
ChungTau <edwardonionc020903@gmail.com>
2024-01-30 04:05:15 +0800

    Update Api

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://chungtau.com:8080/api'||'127.0.0.1:8080/api';
+const API_BASE_URL = 'http://chungtau.com:8080/api';
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
